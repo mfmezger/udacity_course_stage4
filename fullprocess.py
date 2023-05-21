f984fefedd9a32054cdbe8fb7ace6ec2db2f2919
Marc Mezger <marc.mezger@gmail.com>
2023-05-21 15:46:49 +0200

    inital.

--- fullprocess.py
+++ fullprocess.py
@@ -1,42 +1,65 @@
-
-
+import os
+import json
+import subprocess
 import training
 import scoring
 import deployment
 import diagnostics
 import reporting
 
-##################Check and read new data
-#first, read ingestedfiles.txt
 
-#second, determine whether the source data folder has files that aren't listed in ingestedfiles.txt
+def main():
+    with open('config.json', 'r') as f:
+        config = json.load(f)
+
+    input_folder_path = config['input_folder_path']
+    output_folder_path = config['output_folder_path']
+    prod_deployment_path = config['prod_deployment_path']
+
+    deployed_model_path = os.path.join(prod_deployment_path, "trainedmodel.pkl")
+    ingested_files_path = os.path.join(prod_deployment_path, "ingestedfiles.txt")
+    latest_score_path = os.path.join(prod_deployment_path, "latestscore.txt")
+
+    ##################Check and read new data
+    with open(ingested_files_path) as f:
+        ingested_files_list = f.read().splitlines()
+
+    all_files_ingested = all(
+        os.path.join(input_folder_path, file_name) in ingested_files_list
+        for file_name in os.listdir(input_folder_path)
+    )
+
+    ##################Deciding whether to proceed, part 1
+    if not all_files_ingested:
+        print(f"New data found. Ingest new datasets from {input_folder_path}")
+        subprocess.call(['python', 'ingestion.py'])
+    else:
+        print("No new data found. Exit the process")
+        return
+
+    ##################Checking for model drift
+    with open(latest_score_path, 'r') as f:
+        latest_score = float(f.read())
+    new_score = scoring.score_model()
+    print(f'latest score: {latest_score}, score on newly ingested data: {new_score}')
+
+    ##################Deciding whether to proceed, part 2
+    if new_score < latest_score:
+        print('Detect model drift. Retrain and redeploy model')
+    else:
+        print('No model drift happened')
+        return
+
+    ##################Re-deployment
+    subprocess.call(['python', 'training.py'])
+    subprocess.call(['python', 'scoring.py'])
+    subprocess.call(['python', 'deployment.py'])
+
+    ##################Diagnostics and reporting
+    subprocess.call(['python', 'diagnostics.py'])
+    subprocess.call(['python', 'reporting.py'])
+    subprocess.call(['python', 'apicalls.py'])
 
 
-
-##################Deciding whether to proceed, part 1
-#if you found new data, you should proceed. otherwise, do end the process here
-
-
-##################Checking for model drift
-#check whether the score from the deployed model is different from the score from the model that uses the newest ingested data
-
-
-##################Deciding whether to proceed, part 2
-#if you found model drift, you should proceed. otherwise, do end the process here
-
-
-
-##################Re-deployment
-#if you found evidence for model drift, re-run the deployment.py script
-
-
-##################Diagnostics and reporting
-#run diagnostics.py and reporting.py for the re-deployed model
-
-
-
-
-
-
-
-
+if __name__ == '__main__':
+    main()
